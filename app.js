14a3f92e09d49848505e03b732fdce9982931f19
Dikshyanta Aryal <adikshyanta456@gmail.com>
2023-02-11 21:25:52 +0545

    Multiple data passed | checked

--- app.js
+++ app.js
@@ -1,32 +1,44 @@
-const WebSocket = require('ws');
 const express = require('express');
+const sequelize = require('sequelize');
+const app = express();
 
-// const app = express();
+let sensorDataObj = {
+  LED_BLUE : 0,
+  LED_GREEN : 0,
+  LED_RED : 0,
+}
 
-const server = new WebSocket.Server({ port: 3000 });
-
-server.on('connection', (socket) => {
-  console.log('Client connected');
-
-  // let LED_DATA;
-  let datareceived = false;
-
-  socket.on('message', (message) => {
-    // Parse the incoming JSON string
-    if(!datareceived){
-    const data = JSON.parse(message);
-    // const sensor1 = data1
-    // Access the values of the sensor1 and sensor2 keys
-    console.log(data);
-    datareceived = true;
+app.get('/data', async (req, res) => {
+  try
+  {
+    let data1 = await req.query.data1;
+    sensorDataObj.LED_GREEN = data1;
+    let data2 = await req.query.data2;
+    sensorDataObj.LED_BLUE = data2;
+    let data3 = await req.query.data3;
+    sensorDataObj.LED_RED = data3;
+    console.log(data1 + data2 + data3);
+  } catch (err) {
+    console.log(err);
   }
-  });
+  res.send("done");
+});
 
-  // app.use('/',(req,res) => {
-  //   req.json(data);
-  // })
+// app.get('/data1', (req, res) => {
+//   let data = req.query.data;
+//   sensorDataObj.LED_BLUE = data;
+// });
 
-  socket.on('close', () => {
-    console.log('Client disconnected');
-  });
+// app.get('/data2', (req, res) => {
+//   let data = req.query.data;
+//   sensorDataObj.LED_RED = data;
+// });
+
+app.get('/showdata',(req,res) => {
+  console.log(sensorDataObj);
+  res.json(sensorDataObj);
+})
+
+app.listen(3000, () => {
+  console.log('Server listening on port 3000');
 });
--- arduino_main/new.ino
+++ arduino_main/new.ino
@@ -1,45 +1,70 @@
-// #include <ESP8266WiFi.h>
+#include <ESP8266WiFi.h>
 
-// const char* ssid = "Germany";
-// const char* password = "lordbro@555";
-// const char* server = "192.168.16.101";
+const char* ssid = "Germany";
+const char* password = "lordbro@555";
+const char* server = "192.168.16.101";
 
-// void setup() {
-//   Serial.begin(115200);
-//   WiFi.begin(ssid, password);
+void setup() {
+  Serial.begin(115200);
+  WiFi.begin(ssid, password);
 
-//   while (WiFi.status() != WL_CONNECTED) {
-//     delay(1000);
-//     Serial.println("Connecting to WiFi...");
-//   }
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting to WiFi...");
+  }
+  
+  Serial.println("Connected to WiFi");
+}
+  bool datasent = false;
+void loop() {
+  // Define the data you want to send
+  float data = analogRead(A0);
 
-//   Serial.println("Connected to WiFi");
-// }
+  // Connect to the Node.js server
+  WiFiClient client;
+  if (!client.connect(server, 3000)) {
+    Serial.println("Connection failed");
+    return;
+  }else {
+    Serial.println("Connection Succedded");
+  }
+  
+  int counter = 0;
+  float data1,data2,data3;
 
-// void loop() {
-//   // Define the data you want to send
-//   int data = 123;
+  while(counter<3) {
+    if(!datasent) {
+    delay(10000);      
+    if(counter == 0) {
+      data1 = data;
+    } else if(counter == 1) {
+      data2 = data;
+    }
+    else if(counter == 2) {
+      data3 = data;
+      datasent = true;
+      exit(0);
+    }  
+    counter++;  
+  }}
 
-//   // Connect to the Node.js server
-//   WiFiClient client;
-//   if (!client.connect(server, 3000)) {
-//     Serial.println("Connection failed");
-//     return;
-//   }
+  // Send the data as a GET request
+  client.print("GET /data");
+  client.print("?data1=");
+  client.print(data1);
+  client.print("&data2=");
+  client.print(data2);
+  client.print("&data3=");
+  client.print(data3);
+  client.println(" HTTP/1.1");
+  client.println("Host: 192.168.16.1:3000");
+  client.println("Connection: close");
+  client.println();
 
-//   // Send the data as a GET request
-//   client.print("GET /data?data=");
-//   client.print(data);
-//   client.println(" HTTP/1.1");
-//   client.println("Host: 192.168.0.100:3000");
-//   client.println("Connection: close");
-//   client.println();
+  // Read the response from the server
+  while (client.available()) {
+    Serial.write(client.read());
+  }
 
-//   // Read the response from the server
-//   while (client.available()) {
-//     Serial.write(client.read());
-//   }
-
-//   client.stop();
-//   delay(5000);
-// }
+  client.stop();
+}
